3550e45a58c59f3e52e6fcc2864ab577fd79698f
zcesur <17045339+zcesur@users.noreply.github.com>
2020-07-07 13:59:54 +0300

    Implement predicates for straight & flush

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE TypeOperators              #-}
 
 module Lib
   ( someFunc
@@ -10,6 +11,7 @@ import           Text.Parsec.String
 import           Text.Parsec.Char
 import           Text.Parsec.Combinator
 
+import           GHC.TypeLits
 import           Data.Finite
 import qualified Data.Vector.Sized             as V
 import           Data.Vector.Sized              ( (//) )
@@ -19,11 +21,24 @@ import           Control.Applicative            ( (<|>) )
 import           Control.Monad.State
 
 data Suit = Spades | Clubs | Diamonds | Hearts deriving (Show, Eq, Ord)
-data Rank = R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | J | Q | K | A deriving (Show, Eq, Ord)
+data Rank = R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | J | Q | K | A deriving (Show, Eq, Ord, Enum)
 data Card = Card Rank Suit deriving (Show, Eq, Ord)
 
 type HandIndex = Finite 5
 type Hand = V.Vector 5 Card
+type Ranks n = V.Vector n Rank
+type Suits n = V.Vector n Suit
+
+data HandEval = StraightFlush Rank
+              | Quads Rank Rank
+              | FullHouse Rank Rank
+              | Flush (Ranks 5)
+              | Straight Rank
+              | Trips Rank (Ranks 2)
+              | TwoPair Rank Rank Rank
+              | OnePair Rank (Ranks 3)
+              | HighCard (Ranks 5)
+              deriving (Show)
 
 handIndices :: V.Vector 5 HandIndex
 handIndices = V.generate id
@@ -76,5 +91,37 @@ sort cs = snd $ execState (mapM_ move handIndices) (cs, cs)
   move :: HandIndex -> State (Hand, Hand) ()
   move i = pop >>= push i
 
+toRank :: Card -> Rank
+toRank (Card r _) = r
+
+toSuit :: Card -> Suit
+toSuit (Card _ s) = s
+
+same :: (Eq a) => V.Vector (1 + n) a -> Bool
+same xs = all (== V.head xs) xs
+
+isFlush :: Suits 5 -> Bool
+isFlush = same
+
+-- TODO: Implement five-high straight
+isStraight :: Ranks 5 -> Bool
+isStraight rs = same sums
+  where sums = V.imap (\i r -> fromInteger (getFinite i) + fromEnum r) rs
+
+eval :: Hand -> HandEval
+eval cs | isStraight rs && isFlush ss = StraightFlush high
+        | isFlush ss                  = Flush rs
+        | isStraight rs               = Straight high
+        | otherwise                   = HighCard rs
+ where
+  high = V.head rs
+  rs   = V.map toRank cs'
+  ss   = V.map toSuit cs'
+  cs'  = sort cs
+
 someFunc :: IO ()
-someFunc = print $ sort . fst <$> parse hands "8C TS KC 9H 4S 7D 2S 5D 3S AC"
+someFunc = do
+  print $ eval <$> parse hand "KC TC QC 9C JC"
+  print $ eval <$> parse hand "8S TS KS 9S 4S"
+  print $ eval <$> parse hand "8S TC QH 9D JC"
+  print $ eval <$> parse hand "8C TS KC 9H 4S"
