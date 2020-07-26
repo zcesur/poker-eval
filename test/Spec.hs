42649f3e5db9c3ec5153a785a792162baa02f6b2
zcesur <17045339+zcesur@users.noreply.github.com>
2020-07-26 15:10:48 +0300

    Add test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import           Test.Hspec
+import           Text.Parsec                    ( parse )
+import           Data.List                      ( foldl' )
+import           PokerEval
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "PokerEval" $ do
+    it ("correctly determines how many hands Player 1 wins in " ++ fname) $ do
+      f <- readFile fname
+      (foldl' tally 0 . map ord . lines) f `shouldBe` 376
+ where
+  fname = "poker.txt"
+  ord   = fmap (uncurry compare) . parse hands fname
+  tally acc x = if x == Right GT then acc + 1 else acc
